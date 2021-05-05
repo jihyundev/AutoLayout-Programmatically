aff33f40ec2f8e8ffaba0c38a506c1c5be1ad01f
jihyundev <aawlgus12@gmail.com>
2021-05-05 22:25:13 +0900

    add rocket launch animation

--- AutolayoutPractice/AutolayoutPractice/AnimationViewController.swift
+++ AutolayoutPractice/AutolayoutPractice/AnimationViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class AnimationViewController: UIViewController {
     
+    var boxBottomConstraint: NSLayoutConstraint?
+    
     var exitButton: UIButton = {
         let button = UIButton()
         button.setImage(UIImage(named: "exitButton"), for: .normal)
@@ -22,13 +24,13 @@ class AnimationViewController: UIViewController {
     }
     
     var upButton: UIButton = {
-        let button = UIButton()
+        let button = UIButton(type: .system)
         button.setTitle("위로 올리기", for: .normal)
         button.setTitleColor(.black, for: .normal)
         button.titleLabel?.font = .systemFont(ofSize: 30, weight: .bold)
-        button.contentEdgeInsets = UIEdgeInsets(top: 2, left: 5, bottom: 2, right: 5)
+        button.contentEdgeInsets = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
         button.backgroundColor = .white
-        button.layer.cornerRadius = 5
+        button.layer.cornerRadius = 8
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }()
@@ -43,7 +45,7 @@ class AnimationViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = #colorLiteral(red: 0.9607843161, green: 0.7058823705, blue: 0.200000003, alpha: 1)
+        view.backgroundColor = #colorLiteral(red: 0.9686274529, green: 0.78039217, blue: 0.3450980484, alpha: 1)
         
         self.view.addSubview(exitButton)
         exitButton.widthAnchor.constraint(equalToConstant: 100).isActive = true
@@ -51,18 +53,33 @@ class AnimationViewController: UIViewController {
         exitButton.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 20).isActive = true
         exitButton.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 20).isActive = true
         
+        upButton.addTarget(self, action: #selector(moveBoxUp), for: .touchUpInside)
         self.view.addSubview(upButton)
         NSLayoutConstraint.activate([
             upButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            upButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 100)
+            //upButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 100)
+            upButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 150)
         ])
         
         
         self.view.addSubview(box)
-        box.widthAnchor.constraint(equalToConstant: 100).isActive = true
-        box.heightAnchor.constraint(equalToConstant: 100).isActive = true
-        box.topAnchor.constraint(equalTo: upButton.bottomAnchor, constant: 50).isActive = true
+        box.widthAnchor.constraint(equalToConstant: 150).isActive = true
+        box.heightAnchor.constraint(equalToConstant: 50).isActive = true
         box.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        //box.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+        
+        boxBottomConstraint = NSLayoutConstraint(item: box, attribute: .bottom, relatedBy: .equal, toItem: self.view.safeAreaLayoutGuide, attribute: .bottom, multiplier: 1.0, constant: 0)
+        boxBottomConstraint?.isActive = true
+        boxBottomConstraint?.constant = -100
+        
+    }
+    
+    @objc fileprivate func moveBoxUp() {
+        print(#function)
+        boxBottomConstraint?.constant -= 100
+        UIViewPropertyAnimator(duration: 0.3, curve: .easeOut) {
+            self.view.layoutIfNeeded()
+        }.startAnimation()
     }
     
 
@@ -86,6 +103,7 @@ struct AnimationViewControllerRepresentable: UIViewControllerRepresentable {
 struct AnimationViewController_Previews: PreviewProvider {
     static var previews: some View {
         AnimationViewControllerRepresentable()
+            .edgesIgnoringSafeArea(.all)
             .previewDisplayName("아이폰")
     }
 }
--- AutolayoutPractice/AutolayoutPractice/RocketViewController.swift
+++ AutolayoutPractice/AutolayoutPractice/RocketViewController.swift
@@ -17,6 +17,18 @@ class RocketViewController: UIViewController {
         return button
     }()
     
+    var launchButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Launch", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 30, weight: .bold)
+        button.contentEdgeInsets = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+        button.backgroundColor = .white
+        button.layer.cornerRadius = 8
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
     @objc func dismissView() {
         self.dismiss(animated: true, completion: nil)
     }
@@ -24,9 +36,11 @@ class RocketViewController: UIViewController {
     lazy var rocket1: UIImageView = {
         let imageView = UIImageView()
         imageView.image = UIImage(named: "rocket_top_1")
-        //imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.translatesAutoresizingMaskIntoConstraints = false
         return imageView
     }()
+    
+    var rocketBottomConstraint: NSLayoutConstraint?
     
     lazy var rocket2: UIImageView = {
         let imageView = UIImageView()
@@ -61,32 +75,39 @@ class RocketViewController: UIViewController {
         super.viewDidLoad()
         self.view.backgroundColor = #colorLiteral(red: 0.9764705896, green: 0.850980401, blue: 0.5490196347, alpha: 1)
         
+        self.view.addSubview(rocket1)
+        rocket1.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        rocket1.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 0.8).isActive = true
+        rocket1.heightAnchor.constraint(equalTo: self.view.heightAnchor, multiplier: 0.8).isActive = true
+        rocketBottomConstraint = NSLayoutConstraint(item: rocket1, attribute: .bottom, relatedBy: .equal, toItem: self.view.safeAreaLayoutGuide, attribute: .bottom, multiplier: 1.0, constant: 0)
+        rocketBottomConstraint?.isActive = true
+        
         self.view.addSubview(exitButton)
         exitButton.widthAnchor.constraint(equalToConstant: 100).isActive = true
         exitButton.heightAnchor.constraint(equalToConstant: 100).isActive = true
         exitButton.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 40).isActive = true
         exitButton.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 40).isActive = true
-        
         /*
-        let scrollView: UIScrollView = {
-            let scrollView = UIScrollView()
-            scrollView.showsHorizontalScrollIndicator = false
-            scrollView.isPagingEnabled = true
-            scrollView.contentSize = CGSize(width: view.frame.width * CGFloat((views.count)), height: view.frame.height)
-            for i in 0..<views.count {
-                scrollView.addSubview(views[i])
-                views[i].frame = CGRect(x: view.frame.width * CGFloat(i), y: 0, width: view.frame.width, height: view.frame.height)
-            }
-            scrollView.translatesAutoresizingMaskIntoConstraints = false
-            return scrollView
-        }()*/
-        
         self.view.addSubview(scrollView)
         NSLayoutConstraint.activate([
             scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             scrollView.topAnchor.constraint(equalTo: view.topAnchor),
             scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])*/
+        
+        launchButton.addTarget(self, action: #selector(launch), for: .touchUpInside)
+        self.view.addSubview(launchButton)
+        NSLayoutConstraint.activate([
+            launchButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            launchButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20)
         ])
+    }
+    
+    @objc fileprivate func launch() {
+        self.rocketBottomConstraint?.constant -= 900
+        UIViewPropertyAnimator(duration: 0.3, curve: .easeOut) {
+            self.view.layoutIfNeeded()
+        }.startAnimation()
     }
 }
